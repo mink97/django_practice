dc0623df0a8b474d7b69fde26ba8ea3f9a51098c
mingkang <mingkang@student.42seoul.kr>
2024-04-15 12:59:11 +0900

    feat: todo 생성 기능 추가

--- DRF-Projects/03_장고Todo/todo/views.py
+++ DRF-Projects/03_장고Todo/todo/views.py
@@ -9,3 +9,14 @@ def todo_list(request):
 def todo_detail(request, pk):
     todo = Todo.objects.get(id=pk)
     return render(request, 'todo/todo_detail.html', {'todo': todo})
+
+def todo_post(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_post.html', {'form': form})
