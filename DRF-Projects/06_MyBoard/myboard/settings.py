bad5dc3b68a8d7a4d1243791803a433c1c38989f
mingkang <mingkang@student.42seoul.kr>
2024-04-18 12:46:45 +0900

    feat: 필터링 기능 추가

--- DRF-Projects/06_MyBoard/myboard/settings.py
+++ DRF-Projects/06_MyBoard/myboard/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
 
 # Internationalization
--- DRF-Projects/06_MyBoard/posts/views.py
+++ DRF-Projects/06_MyBoard/posts/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from users.models import Profile
@@ -9,6 +10,8 @@ from .serializers import PostSerializer, PostCreateSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [CustomReadonly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author', 'likes']
 
     def get_serializer_class(self):
         if self.action == 'list' or 'retrieve':
